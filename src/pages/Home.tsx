b6ea532abc447f67257a1ad4292a833c9f910038
Mir Sadat Bin Rakib <sadatmir13@gmail.com>
2025-09-15 11:03:22 -0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -89,7 +89,7 @@ const Home = ({ setPage }: Props) => {
                         color={accentColor}
                         mb={{ base: 6, md: 10 }}
                     >
-                        Founder & CEO @CashVA | Algoverse AI Research Fellow ’25 | Harvard HUVTSP ’25 Invitee | AI Full Stack Developer |
+                        Founder & CEO @CashVA | Algoverse AI Research Fellow ’25 | Harvard HUVTSP ’25 Fellow | AI Full Stack Developer |
                     </Text>
                 </Box>
 
